593b9b05a66fc63a899f6329b78f8d9c6cce62c1
northyear <zijz@qq.com>
2022-08-07 02:12:56 -0400

    Fix(packer): quit after first time bootstrap

--- init.lua
+++ init.lua
@@ -5,6 +5,11 @@ if need_bootstrap then
     PACKER_BOOTSTRAP_SUCCESS =
         vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', packer_path }
     vim.cmd.packadd 'packer.nvim'
+    local autocmd = vim.api.nvim_create_autocmd
+    autocmd('User', {
+        pattern = 'PackerComplete',
+        command = 'quitall',
+    })
 else
     require('impatient').enable_profile()
 end
